2812d3aa30a326bff9107f4555bb61da9a485b14
Mark Guzman <segfault@hasno.info>
2020-08-17 17:15:04 -0400

    Add nys settings (#14)
    initial nys settings based on the settings from other deployments

---
+++ migrations/202008131450.do.nys-settings.sql
@@ -0,0 +1,8 @@
+INSERT INTO settings(settings_key, settings_value) VALUES('exposureConfig', '{\"minimumRiskScore\":1,\"attenuationLevelValues\":[2,3,4,5,6,7,8,8],\"attenuationWeight\":1,\"daysSinceLastExposureLevelValues\":[1,1,1,1,1,1,1,1],\"daysSinceLastExposureWeight\":1,\"durationLevelValues\":[1,1,1,1,1,1,1,1],\"durationWeight\":1,\"transmissionRiskLevelValues\":[1,1,1,1,1,1,1,1],\"transmissionRiskWeight\":1, \"durationAtAttenuationThresholds\": [56,62],\"thresholdWeightings\":[1,1,0],\"timeThreshold\":15}');
+INSERT INTO settings(settings_key, settings_value) VALUES('exposureCheckInterval', '120');
+INSERT INTO settings(settings_key, settings_value) VALUES('storeExposuresFor', '14');
+INSERT INTO settings(settings_key, settings_value) VALUES('checkInMaxAge', '28');
+INSERT INTO settings(settings_key, settings_value) VALUES('minAge', '16');
+INSERT INTO settings(settings_key, settings_value) VALUES('riskGroupMinAge', '60');
+INSERT INTO settings(settings_key, settings_value) VALUES('fileLimit', '1');
+INSERT INTO settings(settings_key, settings_value) VALUES('fileLimitiOS', '3');
