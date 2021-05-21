fcab02f40864d57560ec4120add23ca8b6e213e4
Colm Harte <colm.harte@nearform.com>
2021-05-21 10:20:56 +0100

    Delete 202008290730.do.nys-fix-settings.sql

--- migrations/202008290730.do.nys-fix-settings.sql
+++
@@ -1,3 +0,0 @@
-UPDATE settings set is_language = true;
-DELETE FROM settings WHERE settings_key = 'exposureConfig';
-INSERT INTO settings(settings_key, settings_value, is_language) VALUES('exposureConfig', '{\"minimumRiskScore\":1,\"attenuationLevelValues\":[2,3,4,5,6,7,8,8],\"attenuationWeight\":1,\"daysSinceLastExposureLevelValues\":[1,1,1,1,1,1,1,1],\"daysSinceLastExposureWeight\":1,\"durationLevelValues\":[1,1,1,1,1,1,1,1],\"durationWeight\":1,\"transmissionRiskLevelValues\":[1,1,1,1,1,1,1,1],\"transmissionRiskWeight\":1, \"durationAtAttenuationThresholds\": [56,62],\"thresholdWeightings\":[1,1,0],\"timeThreshold\":10}', false);
